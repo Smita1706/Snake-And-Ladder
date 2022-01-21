c02d726e0a8848898cff6d08b7a331e17da1a69c
Smita1706 <smitachinchkar1998@gmail.com>
2022-01-21 22:28:27 +0530

    Added : Use case 1 of Snake and Ladder is added successfully in UC-1SetStartPosition branch

---
+++ SnakeAndLadderUC1.java
@@ -0,0 +1,9 @@
+package SnackAndLadder;
+
+public class SnakeAndLadderUC1 {
+	public static final int START = 0;
+	public static void main(String [] args){
+		System.out.println("Starting position of Snake and Ladder is :" + START);
+
+	}
+}
